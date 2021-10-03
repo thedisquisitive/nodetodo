e03bb229fa7d73e5032319de1fb1b0f746ece8b6
Zach Hogarth <zach@thedisquisitive.com>
2021-10-03 15:59:45 -0400

    Fixed broken file stage

--- app.js
+++ app.js
@@ -1,4 +1,55 @@
-var msg = "Goodbye, cruel world!";
-console.log(msg);
-msg = "Oh hello again";
-console.log(msg);
+class Model {
+    // Initiate a new Model
+    constructor() {
+        // Create a placeholder list
+        this.items = [
+            {id: 1, text: "Finish this app", complete: false},
+        ]
+    }
+
+    // Add an item to the Todo List
+    addItem(itemText) {
+
+    }
+
+    // Edit an item
+    editItem(item_id, editedText) {
+
+    }
+
+    // Delete an item that we don't need anymore
+    deleteItem(item_id) {
+
+    }
+
+    // Toggle completion status of item
+    toggleItem(item_id) {
+
+    }
+}
+
+class View {
+    // Initiate a view
+    constructor() {
+
+    }
+
+    // Create a DOM element; Apply custom CSS here?
+    createElement() {
+        
+    }
+
+    // Get an element from the DOM
+    getElement(tag) {
+
+    }
+}
+
+class Controller {
+    constructor(model, view) {
+        this.model = model;
+        this.view = view;
+    }
+}
+
+const app = new Controller(new Model(), new View());
