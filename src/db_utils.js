11bc04c1f97a38368fa1ccfb6dd1ffa1568d7381
AcrylicRobin <acrylicpuzzles@gmail.com>
2021-10-04 13:41:39 -0400

    committed skeleton utils file

---
+++ src/db_utils.js
@@ -0,0 +1,19 @@
+class Utils {
+	//helpful helper functions for a databse
+	
+	function connect() {
+		//connects to a databse
+	}
+	
+	function exec_sql_file(path) {
+		//reads an sql file and executes it
+	}
+	
+	function exec_result(sql) {
+		//returns the result of a query
+	}
+	
+	function exec_commit(sql) {
+		//commits the given sql
+	}
+}
