66ebbdf3bc107f9bb1201bec6ec014ce51258292
AcrylicRobin <acrylicpuzzles@gmail.com>
2021-10-04 16:02:06 -0400

    added connection and a garbage test

--- src/db_utils.js
+++ src/db_utils.js
@@ -1,8 +1,26 @@
-class Utils {
+class DB_Utils {
 	//helpful helper functions for a databse
+	
+	constructor(host = "localhost", user = "username", password = "") {
+		this.host = host;
+		this.user = user;
+		this.password = password;
+	}
 	
 	function connect() {
 		//connects to a databse
+		var mysql = require('mysql');
+		
+		var con = mysql.createConnection({
+			host: this.host,
+			user: this.user,
+			password: this.password
+		});
+		
+		con.connect(function(err) {
+			if (err) throw err;
+			console.log("Connected!");
+		});
 	}
 	
 	function exec_sql_file(path) {
